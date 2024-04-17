3400af98c296f967165d8e821b1ee842ae01937d
jinexus1 <97098206+jinexus1@users.noreply.github.com>
2024-04-17 23:10:33 +0530

    Create 03_conversionOperations.js

---
+++ 01basics/03_conversionOperations.js
@@ -0,0 +1,35 @@
+// let score="33abc"
+
+// console.log(typeof(score));//string
+
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber)//number
+// console.log(valueInNumber);//NaN(not a number)
+// let score=null
+
+// console.log(typeof(score));//object
+
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber)//number
+// console.log(valueInNumber);//0
+
+let score=undefined
+
+console.log(typeof(score));//string
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber)//string
+console.log(valueInNumber);//NaN(not a number)
+
+
+//"33"=>33
+//"33abc"=>NaN
+// true=> 1 and false=> 0
+
+let loggedIn=-1100.0s
+let booleanIsLoggedIn=Boolean(loggedIn)
+console.log(booleanIsLoggedIn);
+//1=>true and 0=>false
+//""=>false
+//"jin"=>true
+//any real value except 0 gives true in case of numeric value
