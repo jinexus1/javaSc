9475366ecc2552f4a6ca4f71cb67d97f266aea1d
jinexus1 <97098206+jinexus1@users.noreply.github.com>
2024-04-17 21:41:23 +0530

    Create 01_variables.js

---
+++ 01basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId =1445553// cannot be changed
+let accountEmail="hbgribeiur@gmail.com"//block scoped
+var accountPassword="123456"//var is not recommended to use
+accountCity="Knp"
+accountEmail="jj@jj.gmail.com"
+accountPassword="91225"
+accountCity="LKN"
+let accountState;
+// console.log(accountEmail)
+// console.log(accountId)
+// console.log(accountPassword)
+// console.log(accountCity)
+// //better way to print all these variables
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
+console.log(accountState);
+
+
