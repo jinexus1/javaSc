c70fe251af352128f0f34783a3355ca39bc64b7d
jinexus1 <97098206+jinexus1@users.noreply.github.com>
2024-04-17 21:42:12 +0530

    Create 02_dataTypes.js

---
+++ 01basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all JS code as newer version
+alert(3+3)
+//while using alert function is vscode or another code edditor
+//it will print an error while usinf developers console in browser
+//it will work fine beacuse in browser js is bind withit
+//but in vscode it used as js runtime environment
+//To use alert in code edditor we have a different syntax
+
+//number 0<=range<2^53\
+//bigint
+//string= ""
+//boolean=true or false
+//null= stand alone value
+/* basically null is used to give when no value is provided
+where as undefined means the value was never give to the variale*/
+
+console.log(typeof null);//obeject
+console.log(typeof undefined);//undefined
