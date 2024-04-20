46e6718b53d2243f425687b8b0ac5bf764349c33
jinexus1 <97098206+jinexus1@users.noreply.github.com>
2024-04-20 22:27:54 +0530

    Create 05_ObjectDestructuring.js

---
+++ 02Basics/05_ObjectDestructuring.js
@@ -0,0 +1,16 @@
+//Object Destructuing
+const course={
+  coursename:"js in hindi",
+  price:"1000",
+  courseInstructor:"Archit"
+}
+const {courseInstructor:instructor}=course
+console.log(instructor);
+//Array Destructuing
+//Json: It  JavaScript object notation
+// {
+//  " name":"Archit",
+//   "courseName":"JS",
+//   "price":"free",
+// }//this is JSON
+
