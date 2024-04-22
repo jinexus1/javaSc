fc693c6477cea727c27efe7aceab573f3058b998
jinexus1 <97098206+jinexus1@users.noreply.github.com>
2024-04-22 23:09:04 +0530

    Create 03_forOf_map.js

---
+++ 05Iterations/03_forOf_map.js
@@ -0,0 +1,43 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);//1 2 3 4 5(in different line)
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+//Map object holds key value pair and remembers the original insertion order of 
+//the keys, no dupliacte values
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+for (const [key, value] of myObject) {
+    console.log(key, ':-', value);
+    
+}//TypeError: myObejct is not iterable
+//to iterate over objects for of loop gives an error
