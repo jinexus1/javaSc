ea00dee4f26d8fb1eefb05e63b87aeaebd7318d3
jinexus1 <97098206+jinexus1@users.noreply.github.com>
2024-04-25 00:56:54 +0530

    Update index.js
    now the text will be highlited accoring to BMI in weight guide

--- 07Projects/02_BMI_Calculator/index.js
+++ 07Projects/02_BMI_Calculator/index.js
@@ -4,6 +4,9 @@ from.addEventListener('submit',function(e){
   const height=parseInt(document.querySelector('#height').value)
   const weight=parseInt(document.querySelector('#weight').value)
   const results=document.querySelector('#results')
+  const underWeight=document.querySelector('#lessWeight')
+  const normalWeight=document.querySelector('#normalWeight')
+  const overWeight=document.querySelector('#overWeight')
   if(height===''||height<0|| isNaN(height)){
     results.innerHTML=`Please give a valid height ${height}`
   }
@@ -17,12 +20,15 @@ from.addEventListener('submit',function(e){
   
   if(bmi<=18.6){
     results.innerHTML+=`<span>Under Weight </span>`
+    underWeight.style.color='blue'
   }
   if(bmi<=24.9 && bmi>18.6){
     results.innerHTML+=`<span>Normal Weight </span>`
+    normalWeight.style.color='blue'
   }
   if(bmi>=24.9){
     results.innerHTML+=`<span>Over Weight </span>`
+    overWeight.style.color='blue'
   }
 }
 });
