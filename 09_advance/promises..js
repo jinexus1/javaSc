6f8b440f689b2177a44380f6ecf9b629671b33db
jinexus1 <ap2929470@gmail.com>
2024-04-29 22:38:24 +0530

    promises

---
+++ 09_advance/promises..js
@@ -0,0 +1,8 @@
+//The Promise object represents the eventual completion (or failure) of an asynchronous operation and its 
+//resulting value.
+//Examples: cryptography,encryption etc
+//Three States
+//pending: initial state, neither fulfilled nor rejected.
+// fulfilled: meaning that the operation was completed successfully.
+// rejected: meaning that the operation failed.
+const promiseOne=new Promise()
