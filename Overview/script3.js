f323618f499162d53e5a32d8c6056a0d6030fc15
jinexus1 <ap2929470@gmail.com>
2024-08-04 19:47:49 +0530

    fetch, async await

---
+++ Overview/script3.js
@@ -0,0 +1,11 @@
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+}
+
+getTodos();
